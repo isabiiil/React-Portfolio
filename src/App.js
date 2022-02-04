6ed214b546219b032e7886e19c1608f2180eba32
Isabel Abonitalla <isabelabonitalla@gmail.com>
2022-02-04 13:19:26 -0500

    landing and experience

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Experience from './sections/Experience';
+import Landing from './sections/Landing';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <div><Landing /><Experience /></div>;
 }
-
-export default App;
---
+++ src/sections/Landing.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import sunset from '../assets/sunset.jpg';
+import '../App.css';
+
+export default function Landing() {
+
+  const styles={
+    image: {
+      backgroundSize: 'cover',
+      width: '100vw',
+      height: '100vh',
+      position: 'absolute',
+    },
+    center: {
+      margin: '0 auto',
+      fontSize: '4vw'
+    }
+  }
+
+  return (
+    <>
+      <img src={sunset} alt="Pink Sunset from Bonita Springs, Florida" style={styles.image} />
+      <div style={styles.center}>
+        <p>Hi! I'm</p>
+        <div className="scroller">
+          <span>
+            Isabel<br/>
+            a CS undergrad<br />
+            a product manager<br/>
+            a hackathon enthusiast
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
